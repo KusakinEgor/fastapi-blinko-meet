8753c2dbf70f066ab2e8d8f76fcda8f002d6851c
Egor <bananaxvii812@gmail.com>
2025-09-26 19:55:24 +0500

    Create module with init --lib rust

---
+++ media-core/target/debug/deps/media_core-c9f7ec6b8ed532c2.d
@@ -0,0 +1,5 @@
+C:\Users\code80\Desktop\fastapi-blinko-meet\media-core\target\debug\deps\media_core-c9f7ec6b8ed532c2.d: src\lib.rs
+
+C:\Users\code80\Desktop\fastapi-blinko-meet\media-core\target\debug\deps\media_core-c9f7ec6b8ed532c2.exe: src\lib.rs
+
+src\lib.rs:
---
+++ media-core/target/debug/deps/media_core-f7ad722cc6357f85.d
@@ -0,0 +1,7 @@
+C:\Users\code80\Desktop\fastapi-blinko-meet\media-core\target\debug\deps\media_core-f7ad722cc6357f85.d: src\lib.rs
+
+C:\Users\code80\Desktop\fastapi-blinko-meet\media-core\target\debug\deps\libmedia_core-f7ad722cc6357f85.rlib: src\lib.rs
+
+C:\Users\code80\Desktop\fastapi-blinko-meet\media-core\target\debug\deps\libmedia_core-f7ad722cc6357f85.rmeta: src\lib.rs
+
+src\lib.rs:
