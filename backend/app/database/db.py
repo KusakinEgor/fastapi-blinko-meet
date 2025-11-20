0bcc752af1008859c6a71929e2c8becefbcf34d1
Egor <bananaxvii812@gmail.com>
2025-11-20 22:34:06 +0500

    add file db.py which work with session

---
+++ backend/app/database/db.py
@@ -0,0 +1,16 @@
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from app.config import DATABASE_URL
+
+engine = create_async_engine(DATABASE_URL)
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+async def get_db_session() -> AsyncGenerator[AsyncSession]:
+    async with AsyncSessionLocal() as session:
+        yield session
