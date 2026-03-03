d0c2e6208b77be81f0effb0ed32ebdb5e61f4343
KusakinEgor <bananaxvii812@gmail.com>
2026-03-03 20:15:18 +0500

    implement database models for users

---
+++ backend/app/schemas/auth.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import String, Integer
+from app.database.db import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
