a7b8111d0f9c30bd9dc91b006dd142600337bbe8
KusakinEgor <bananaxvii812@gmail.com>
2026-03-05 21:45:32 +0500

    add new field in table users

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -1,10 +1,15 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String, Integer
+from datetime import datetime
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Integer, DateTime, func
 from app.database.db import Base
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "users" 
 
     id: Mapped[int] = mapped_column(primary_key=True)
+
     username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+
+
