46abf5401700b991c63b184eac9265c5ee32fb00
KusakinEgor <bananaxvii812@gmail.com>
2026-03-06 21:49:02 +0500

    update auth.py add field display name

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -2,14 +2,12 @@ from datetime import datetime
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import String, Integer, DateTime, func
 from app.database.db import Base
+from app.schemas.common import TimestampMixin
 
-class User(Base):
+class User(TimestampMixin, Base):
     __tablename__ = "users" 
-
-    id: Mapped[int] = mapped_column(primary_key=True)
 
     username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
-
-
+    display_name: Mapped[str] = mapped_column(String(100), nullable=False)
