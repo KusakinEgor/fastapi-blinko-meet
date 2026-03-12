563860dba0a7a37c755f70e1ee76061e4af7864a
KusakinEgor <bananaxvii812@gmail.com>
2026-03-12 21:52:05 +0500

    Add some models for response in endpoints register and login

--- backend/app/models/auth.py
+++ backend/app/models/auth.py
@@ -3,13 +3,23 @@ from pydantic import BaseModel, EmailStr, Field
 class Registration(BaseModel):
     username: str = Field(..., min_length=1, description="Имя пользователя не должно быть пустым")
     email: EmailStr = Field(..., description="Email обязателен")
-    password: str = Field(..., min_length=6, description="Пароль не может быть пустым и минимум 6 символов")
+    password: str = Field(..., min_length=3, max_length=72, description="Пароль не может быть пустым и минимум 6 символов")
 
-class SuccessRegister(BaseModel):
-    done: bool
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: EmailStr
+    display_name: str | None
+
+    class Config:
+        from_attributes = True
+
+class TokenResponse(BaseModel):
+    access_token: str
+    token_type: str = "bearer"
 
 class Login(BaseModel):
     username: str = Field(..., min_length=1)
-    password: str = Field(..., min_length=6)
+    password: str = Field(..., min_length=3)
     email: EmailStr = Field(..., min_length=5)
 
