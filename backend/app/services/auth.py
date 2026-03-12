7b5da2155f5118eebf97f08c196936c480a80f09
KusakinEgor <bananaxvii812@gmail.com>
2026-03-12 21:54:16 +0500

    Add work with database

--- backend/app/services/auth.py
+++ backend/app/services/auth.py
@@ -1,22 +1,47 @@
 from app.core.security import hash_password, verify_password, create_access_token
+from app.models.auth import Registration, Login
+from app.schemas.auth import User
+
+from fastapi import HTTPException, status
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
-async def register_user_service(db: AsyncSession, password: str) -> str:
-    hashed = hash_password(password)
-    # Здесь нужно создать пользователя в БД, например:
-    # user = User(hashed_password=hashed, ...)
-    # db.add(user)
-    # check this password with data in db
-    # await db.commit()
-    # await db.refresh(user)
-    return hashed
+async def register_user_service(db: AsyncSession, data: Registration) -> User:
+    hashed_password = hash_password(data.password)
 
+    new_user = User(
+            username=data.username,
+            email=data.email,
+            hashed_password=hashed_password
+    )
 
-async def login_user_service(db: AsyncSession, password: str, email: str) -> dict:
-    # user = await get_user_by_email(db, email)
+    db.add(new_user)
 
-    # if not verify_password(password, user.hashed_password):
-    #       raise Value Error
+    try:
+        await db.commit()
+        await db.refresh(new_user)
+        return new_user
+    except Exception as e:
+        await db.rollback()
+        raise e
 
-    token = create_access_token()
+async def login_user_service(db: AsyncSession, data: Login) -> dict:
+    query = select(User).where(User.email == data.email)
+    result = await db.execute(query)
+    user = result.scalars().first()
+
+    if not user:
+        raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Wrong email or password"
+        )
+
+    if not verify_password(data.password, user.hashed_password):
+        raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Wrong email or password"
+        )
+
+    token = create_access_token(user.id)
+
     return {"access_token": token, "token_type": "bearer"}
