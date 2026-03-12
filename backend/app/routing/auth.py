cf7d7e4cc01664f7717c3dc8b4a9ed7824e7228d
KusakinEgor <bananaxvii812@gmail.com>
2026-03-12 21:52:33 +0500

    Change returns in endpoints

--- backend/app/routing/auth.py
+++ backend/app/routing/auth.py
@@ -1,29 +1,28 @@
 from fastapi import APIRouter, Depends
-from app.models.auth import Registration, Login, SuccessRegister
+from app.models.auth import Registration, Login, TokenResponse, UserResponse
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.database.db import get_db_session
 from app.services.auth import login_user_service, register_user_service
 
 router = APIRouter()
 
-@router.post("/register", response_model=SuccessRegister)
+@router.post("/register", response_model=UserResponse)
 async def register(
     data: Registration,
     db: AsyncSession = Depends(get_db_session)
-) -> SuccessRegister:
-    user = await register_user_service(db, data.password)
-    
-    return SuccessRegister(
-        done=True
-    )
+):
+    user = await register_user_service(db, data)
+    return user
 
-@router.post("/login", response_model=SuccessRegister)
+@router.post("/login", response_model=TokenResponse)
 async def login(
     data: Login,
     db: AsyncSession = Depends(get_db_session)
-) -> SuccessRegister:
-    token = await login_user_service(db, data.password, data.email)
-    
-    return SuccessRegister(
-        done=True
+) -> TokenResponse:
+    token_data = await login_user_service(db, data)
+
+    return TokenResponse(
+            access_token=token_data["access_token"],
+            token_type=token_data["token_type"]
     )
+    
