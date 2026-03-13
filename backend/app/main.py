1cf4ad0a07de223bfd0823cfcbee72b40595ea45
KusakinEgor <bananaxvii812@gmail.com>
2026-03-13 14:10:36 +0500

    Add engine dispose when application is end

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,19 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+
+from app.database.db import engine
 
 from app.routing.auth import router as auth_router
 from app.routing.rooms import router as rooms_router
 from app.routing.media import router as media_router
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    await engine.dispose()
+
+app = FastAPI(lifespan=lifespan)
 
 origins = [
     "http://localhost:5173"
