c4e86a695efa6705cf6057df9ca380ae9f68c43c
KusakinEgor <bananaxvii812@gmail.com>
2026-03-14 18:28:10 +0500

    Update response in endpoints -> create and join

--- backend/app/routing/rooms.py
+++ backend/app/routing/rooms.py
@@ -5,15 +5,25 @@ router = APIRouter(prefix="/rooms", tags=["Rooms"])
 
 @router.post("/", response_model=RoomOut)
 async def create_room(room_data: RoomCreate):
-    new_room = {**room_data.dict(), "id": 1, "owner_id": 123}
-    return new_room
+    return {
+            "id": 1,
+            "slug": "some-slug",
+            "name": room_data.name,
+            "host_id": 123,
+            "is_activate": True,
+            "created_at": "2024-01-01T00:00:00"
+    }
 
 @router.post("/{slug}/join", response_model=RoomJoinResponse)
 async def join_room(slug: str):
     if slug == "not-found":
         raise HTTPException(status_code=404, detail="Room not found")
 
-    return {"room_slug": slug, "status": "joined", "user_id": 123}
+    return {
+            "room_slug": slug,
+            "session_token": "test_token-123",
+            "ice_servers": []
+    }
 
 @router.get("/{slug}/participants")
 async def get_participants(slug: str):
