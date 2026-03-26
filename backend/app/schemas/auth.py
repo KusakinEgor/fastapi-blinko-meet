1863eb186ce11f81f40fbb90c150c7fa6a1f8333
KusakinEgor <bananaxvii812@gmail.com>
2026-03-26 16:09:28 +0500

    Update User class

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -1,8 +1,8 @@
 from datetime import datetime
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, Integer, DateTime, func
 from app.database.db import Base
-from app.schemas.common import TimestampMixin
+from .common import TimestampMixin
 
 class User(TimestampMixin, Base):
     """Represents a reqistered system user and their authentication credentials."""
@@ -14,3 +14,6 @@ class User(TimestampMixin, Base):
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
     display_name: Mapped[str] = mapped_column(String(100), nullable=True)
+
+    profile: Mapped["UserProfile"] = relationship("UserProfile", back_populates="user", uselist=False)
+    settings: Mapped["UserSettings"] = relationship("UserSettings", back_populates="user", uselist=False)
