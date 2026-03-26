70a28753a3809419be6a332e98ba22da46913c10
KusakinEgor <bananaxvii812@gmail.com>
2026-03-26 16:08:24 +0500

    Add decode jwt token

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -1,6 +1,7 @@
 from passlib.context import CryptContext
 from datetime import datetime, timedelta, timezone
-from jose import jwt
+from jose import JWTError, jwt
+from typing import Optional
 from app.config import ALGORITHM, SECRET_KEY, ACCESS_TOKEN_EXPIRE_MINUTES
 
 pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
@@ -15,6 +16,17 @@ def create_access_token(user_id: int) -> str:
     expire = datetime.now(timezone.utc) + timedelta(minutes=float(ACCESS_TOKEN_EXPIRE_MINUTES))
     payload = {"sub": str(user_id), "exp": expire}
     return jwt.encode(payload, str(SECRET_KEY), str(ALGORITHM))
+
+def decode_access_token(token: str) -> Optional[int]:
+    try:
+        payload = jwt.decode(token, str(SECRET_KEY), algorithms=[str(ALGORITHM)])
+        user_id_str = payload.get("sub")
+
+        if user_id_str is None:
+            return None
+        return int(user_id_str)
+    except (JWTError, ValueError):
+        return None
 
 # check for self will use that later
 if __name__ == "__main__":
