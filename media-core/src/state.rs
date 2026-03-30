f534abbc922ad6b0f0ea72746498985b2c60a0dd
KusakinEgor <bananaxvii812@gmail.com>
2026-03-30 18:22:58 +0500

    Add sructures

---
+++ media-core/src/state.rs
@@ -0,0 +1,16 @@
+use std::{collections::HashMap, sync::Arc};
+use tokio::sync::{Mutex, broadcast};
+use webrtc::peer_connection::RTCPeerConnection;
+use webrtc::track::track_local::track_local_static_rtp::TrackLocalStaticRTP;
+
+pub struct Participant {
+    pub user_id: String,
+    pub pc: Arc<RTCPeerConnection>,
+    pub sender: broadcast::Sender<String>,
+    pub published_tracks: Mutex<Vec<Arc<TrackLocalStaticRTP>>>,
+}
+
+pub struct AppState {
+    pub api: webrtc::api::API,
+    pub rooms: Mutex<HashMap<String, Vec<Arc<Participant>>>>,
+}
