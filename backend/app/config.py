3f4fbc3c1aa4bc1cbb1b2e1de4ea2505cc05a622
KusakinEgor <bananaxvii812@gmail.com>
2026-04-06 17:59:13 +0500

    Covert to str from .env

--- backend/app/config.py
+++ backend/app/config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = str(os.getenv("DATABASE_URL"))
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
+ALGORITHM = str(os.getenv("ALGORITHM"))
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 15))
