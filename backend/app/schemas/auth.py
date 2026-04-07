e8cb776ab96ad2023b19245d81c846804f5cc0fd
KusakinEgor <bananaxvii812@gmail.com>
2026-04-07 22:25:49 +0500

    Update class User add new fields

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import String, Integer, DateTime, func
+from sqlalchemy import String, Integer, DateTime, func, Boolean, text
 from app.database.db import Base
 from .common import TimestampMixin
 
@@ -11,8 +11,10 @@ class User(TimestampMixin, Base):
     username: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(100), unique=True, nullable=False)
 
+    status: Mapped[str] = mapped_column(String(20), server_default=text("'FREE'"), nullable=False)
+    likes: Mapped[int] = mapped_column(Integer, server_default=text("0"), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, server_default=text("true"), nullable=False)
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
-    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
     display_name: Mapped[str] = mapped_column(String(100), nullable=True)
 
     profile: Mapped["UserProfile"] = relationship("UserProfile", back_populates="user", uselist=False)
