99b3183e440cbabd3f28297152da49b415eaf0c4
KusakinEgor <bananaxvii812@gmail.com>
2026-04-10 23:13:57 +0500

    Add new import from chat

--- backend/app/schemas/__init__.py
+++ backend/app/schemas/__init__.py
@@ -2,3 +2,4 @@ from .auth import User
 from .user import UserProfile, UserSettings
 from .meeting import Rooms, Participants
 from .common import ParticipantRole, TimestampMixin
+from .chat import Message
