63d4f543fbccbf7ce8d3462c00801ddc88ac49a0
KusakinEgor <bananaxvii812@gmail.com>
2026-04-13 20:00:47 +0500

    Add new method for login admin/emp.

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -1,7 +1,29 @@
+const API_BASE_URL = "http://localhost:8000";
+
+export async function loginEmployee(username, password) {
+	const res = await fetch(`${API_BASE_URL}/employee/login`, {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({ email: username, password })
+	});
+
+	if (!res.ok) {
+		const errorData = await res.json();
+		throw new Error(errorData.detail || "Error auth");
+	}
+
+	const data = await res.json();
+
+	localStorage.setItem("access_token", data.access_token);
+	localStorage.setItem("refresh_token", data.refresh_token);
+
+	return data;
+}
+
 export async function refreshAccessToken() {
 	const refreshToken = localStorage.getItem("refresh_token");
 
-	const res = await fetch("http://localhost:8000/refresh", {
+	const res = await fetch(`${API_BASE_URL}/refresh`, {
 		method: "POST",
 		headers: {
 			"x-refresh-token": refreshToken
