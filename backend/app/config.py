a8559e55385ec7570fd3f46c29401211b4d972ca
KusakinEgor <bananaxvii812@gmail.com>
2026-04-15 15:50:24 +0500

    Get new data from .env secrets

--- backend/app/config.py
+++ backend/app/config.py
@@ -8,3 +8,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 ALGORITHM = str(os.getenv("ALGORITHM"))
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 15))
 SECRET_CRYPTO_KEY = os.getenv("SECRET_CRYPTO_KEY", "default_key_for_dev_only=")
+
+GIGA_CLIENT_ID = os.getenv("GIGA_CLIENT_ID")
+GIGA_CLIENT_SECRET = os.getenv("GIGA_CLIENT_SECRET")
+GIGA_AUTH_KEY = os.getenv("GIGA_AUTH_KEY")
