9ecd00026b110950c960d36299afd3fea3bf9544
KusakinEgor <bananaxvii812@gmail.com>
2026-04-20 21:37:45 +0500

    Update User model add badges

--- backend/app/schemas/auth.py
+++ backend/app/schemas/auth.py
@@ -1,8 +1,10 @@
+from typing import List
 from datetime import datetime
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import String, Integer, DateTime, func, Boolean, text
 from app.database.db import Base
 from .common import TimestampMixin
+from .badge import user_badges
 
 class User(TimestampMixin, Base):
     """Represents a reqistered system user and their authentication credentials."""
@@ -16,6 +18,7 @@ class User(TimestampMixin, Base):
     is_active: Mapped[bool] = mapped_column(Boolean, server_default=text("true"), nullable=False)
     hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
     display_name: Mapped[str] = mapped_column(String(100), nullable=True)
+    badges: Mapped[List["Badge"]] = relationship("Badge", secondary=user_badges, backref="users", lazy="selectin")
 
     profile: Mapped["UserProfile"] = relationship("UserProfile", back_populates="user", uselist=False)
     settings: Mapped["UserSettings"] = relationship("UserSettings", back_populates="user", uselist=False)
