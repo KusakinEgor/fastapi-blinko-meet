85df1de70d5946994d4903dc1de6aa8d26350d0e
KusakinEgor <bananaxvii812@gmail.com>
2026-04-22 20:41:02 +0500

    Add class to handle rooms and broadcasting

---
+++ backend/app/services/audio_manager.py
@@ -0,0 +1,21 @@
+from fastapi import WebSocket
+from typing import Dict, List
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: Dict[str, List[WebSocket]] = {}
+
+    async def connect(self, websocket: WebSocket, room_id: str):
+        await websocket.accept()
+        self.active_connections.setdefault(room_id, []).append(websocket)
+
+    def disconnect(self, websocket: WebSocket, room_id: str):
+        if room_id in self.active_connections:
+            self.active_connections[room_id].remove(websocket)
+
+    async def broadcast(self, message: bytes, room_id: str, sender: WebSocket):
+        for connection in self.active_connections.get(room_id, []):
+            if connection != sender:
+                await connection.send_bytes(message)
+
+audio_manager = ConnectionManager()
