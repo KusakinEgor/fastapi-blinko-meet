a5cf544affdea21fa4529cf8a54fd680d1a78748
KusakinEgor <bananaxvii812@gmail.com>
2026-05-22 18:35:46 +0500

    Update peer.rs add stun server for ice

--- media-core/src/rtc/peer.rs
+++ media-core/src/rtc/peer.rs
@@ -2,11 +2,23 @@ use std::sync::Arc;
 use webrtc::{
     api::API,
     peer_connection::{RTCPeerConnection, configuration::RTCConfiguration},
+    ice_transport::ice_server::RTCIceServer,
 };
 
 pub async fn create_peer(api: &API) -> Arc<RTCPeerConnection> {
-    println!("Creating PeerConnection");
+    println!("Creating PeerConnection with NAT Engine Configuration");
+
+    let config = RTCConfiguration {
+        ice_servers: vec![
+            RTCIceServer {
+                urls: vec!["stun:stun.l.google.com:19302".to_string()],
+                ..Default::default()
+            }
+        ],
+        ..Default::default()
+    };
+    
     Arc::new(
-        api.new_peer_connection(RTCConfiguration::default()).await.unwrap()
+        api.new_peer_connection(config).await.unwrap()
     )
 }
