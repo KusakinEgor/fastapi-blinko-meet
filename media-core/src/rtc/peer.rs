0420fad947e3098266b4a8e8a644a027f0c1396e
KusakinEgor <bananaxvii812@gmail.com>
2026-05-22 18:40:56 +0500

    Reset file peer.rs

--- media-core/src/rtc/peer.rs
+++ media-core/src/rtc/peer.rs
@@ -2,23 +2,12 @@ use std::sync::Arc;
 use webrtc::{
     api::API,
     peer_connection::{RTCPeerConnection, configuration::RTCConfiguration},
-    ice_transport::ice_server::RTCIceServer,
 };
 
 pub async fn create_peer(api: &API) -> Arc<RTCPeerConnection> {
-    println!("Creating PeerConnection with NAT Engine Configuration");
-
-    let config = RTCConfiguration {
-        ice_servers: vec![
-            RTCIceServer {
-                urls: vec!["stun:stun.l.google.com:19302".to_string()],
-                ..Default::default()
-            }
-        ],
-        ..Default::default()
-    };
-    
+    println!("Creating PeerConnection");
     Arc::new(
-        api.new_peer_connection(config).await.unwrap()
+        api.new_peer_connection(RTCConfiguration::default()).await.unwrap()
     )
 }
+
