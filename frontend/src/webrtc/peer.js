75e5ed15b99f2c97c8d2d43cfcc946aa6ec75daa
KusakinEgor <bananaxvii812@gmail.com>
2026-05-24 20:23:52 +0500

    Update turn delete : in address

--- frontend/src/webrtc/peer.js
+++ frontend/src/webrtc/peer.js
@@ -3,7 +3,7 @@ export function createPeer({ localStream, onTrack, onIceCandidate }) {
 		iceServers: [
 			{ urls: "stun:stun.l.google.com:19302" },
 			{
-				urls: "turn:77.110.125.7:3478:?transport=udp",
+				urls: "turn:77.110.125.7:3478?transport=udp",
 				username: "coolserver",
 				credential: "niceserver"
 			}
