f125ca02e37dc8d476a512628399de26bcf84cb6
KusakinEgor <bananaxvii812@gmail.com>
2026-05-31 11:36:27 +0500

    Add new field room_id

--- backend/app/models/chat.py
+++ backend/app/models/chat.py
@@ -3,11 +3,13 @@ from datetime import datetime
 
 class MessageCreate(BaseModel):
     content: str = Field(..., min_length=1, examples=["Hello everyone!"], description="The text content of the message")
+    room_id: str
 
 class MessageResponse(BaseModel):
     id: int = Field(..., examples=[101])
     content: str = Field(..., examples=["Hello everyone!"])
     user_id: int = Field(..., examples=[1])
+    room_id: str | None
     created_at: datetime = Field(..., examples=["2023-10-27T10:00:00"])
 
     class Config:
